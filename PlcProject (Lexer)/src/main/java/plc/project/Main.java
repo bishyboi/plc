174dd8a79c8e8376220e759bc8cead6814ee7874
Bishoy <bishoypramanik@gmail.com>
2026-02-24 21:19:23 -0500

    combining parser and lexer

--- PlcProject (Lexer)/src/main/java/plc/project/Main.java
+++ PlcProject (Lexer)/src/main/java/plc/project/Main.java
@@ -2,8 +2,13 @@ package plc.project;
 
 import plc.project.lexer.LexException;
 import plc.project.lexer.Lexer;
+import plc.project.parser.ParseException;
+import plc.project.parser.Parser;
 
 import java.util.Scanner;
+import java.util.function.Function;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 
 /**
  * Provides an entry point to a REPL (Read-Eval-Print-Loop) for each part of our
@@ -13,10 +18,10 @@ import java.util.Scanner;
 public final class Main {
 
     private interface Repl {
-        void evaluate(String input) throws LexException;
+        void evaluate(String input) throws LexException, ParseException;
     }
 
-    private static final Repl REPL = Main::lexer; //edit for manual testing
+    private static final Repl REPL = Main::parser; //edit for manual testing
 
     static void main() {
         System.out.println("REPL running, enter empty line for multiline input.");
@@ -24,7 +29,7 @@ public final class Main {
             var input = readInput();
             try {
                 REPL.evaluate(input);
-            } catch (LexException e) {
+            } catch (LexException | ParseException e) {
                 System.out.println(e.getClass().getSimpleName() + ": " + e.getMessage());
             } catch (RuntimeException e) {
                 e.printStackTrace(System.err);
@@ -38,6 +43,12 @@ public final class Main {
         for (var token : tokens) {
             System.out.println(" - " + token);
         }
+    }
+
+    private static void parser(String input) throws LexException, ParseException {
+        var tokens = new Lexer(input).lex();
+        var ast = new Parser(tokens).parse("source"); //edit for manual testing
+        System.out.println(prettify(ast.toString()));
     }
 
     private static final Scanner SCANNER = new Scanner(System.in);
@@ -55,6 +66,37 @@ public final class Main {
             next = SCANNER.nextLine();
         }
         return builder.toString();
+    }
+
+    private static final Pattern RECORD_FORMAT = Pattern.compile(
+        "(?<open>)(?<=^|\\[|, |=)[A-Za-z]*\\[(?<inline>(?=[^\\[]*?]))?" +
+            "|(?<value>), (?=[A-Za-z]+(\\[|=))" +
+            "|(?<close>)](?=,|]|$)"
+    );
+
+    private static String prettify(String record) {
+        //This is not how you're supposed to use regex, but... it works?
+        return RECORD_FORMAT.matcher(record).replaceAll(new Function<>() {
+            private String indent = "";
+            private boolean inline = false;
+            @Override
+            public String apply(MatchResult m) {
+                if (inline || m.group("inline") != null) {
+                    inline = m.group("close") == null;
+                    return m.group();
+                } else if (m.group("open") != null) {
+                    indent = indent + "    ";
+                    return m.group() + "\n" + indent;
+                } else if (m.group("value") != null) {
+                    return ",\n" + indent + m.group().replaceFirst(", ", "");
+                } else if (m.group("close") != null) {
+                    indent = indent.replaceFirst("    ", "");
+                    return "\n" + indent + m.group();
+                } else {
+                    throw new AssertionError(m.group());
+                }
+            }
+        });
     }
 
 }
